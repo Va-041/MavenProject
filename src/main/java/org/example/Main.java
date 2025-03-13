0f1d015313a3b1caeffcc0babba95e39a1e31539
Va-041 <vladyaserika@mail.ru>
2025-03-13 17:03:35 +0300

    Something ?..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\n\n\t\t==\tMy first maven project\t==");
     }
 }
